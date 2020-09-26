fc649268b865ce543e76340a05cd2b7c88bb28de
Abhishek Kumar <abhishek@hotstar.com>
2020-09-27 00:36:27 +0530

    Updated the script with correct file name

--- provisioning/import_provisioning.sh
+++ provisioning/import_provisioning.sh
@@ -6,7 +6,7 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 # cp provisioning/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 
 security create-keychain -p "" build.keychain
-security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$PROVISIONING_PASSWORD" -A
+security import provisioning/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$PROVISIONING_PASSWORD" -A
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
